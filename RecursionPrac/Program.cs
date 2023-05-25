09a059b03c75da50c7ad801fd8c3a6fc8b9f6bd5
Hanul Kim <s2122@e-mirim.hs.kr>
2023-05-25 10:10:08 +0900

    메모화

--- RecursionPrac/Program.cs
+++ RecursionPrac/Program.cs
@@ -11,11 +11,10 @@ namespace RecursionPrac
         static void Main(string[] args)
         {
             Fibonacci fibo = new Fibonacci();
-            Console.WriteLine(fibo.Get(1));
-            Console.WriteLine(fibo.Get(2));
-            Console.WriteLine(fibo.Get(3));
-            Console.WriteLine(fibo.Get(4));
-           
+            Console.WriteLine(fibo.Get(30));
+            
+            FibonacciAdv fiboAdv = new FibonacciAdv();
+            Console.WriteLine(fiboAdv.Get(50000));
         }
     }
 }
