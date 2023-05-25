7c8ccb66a7f8fffa0b68026de1c5e3c5fc7676aa
Hanul Kim <s2122@e-mirim.hs.kr>
2023-05-25 09:59:11 +0900

    피보나치 수 재귀 메서드

--- RecursionPrac/Program.cs
+++ RecursionPrac/Program.cs
@@ -10,6 +10,12 @@ namespace RecursionPrac
     {
         static void Main(string[] args)
         {
+            Fibonacci fibo = new Fibonacci();
+            Console.WriteLine(fibo.Get(1));
+            Console.WriteLine(fibo.Get(2));
+            Console.WriteLine(fibo.Get(3));
+            Console.WriteLine(fibo.Get(4));
+           
         }
     }
 }
